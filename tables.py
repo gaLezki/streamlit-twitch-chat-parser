7f1d0b14c8225b74f700b58c315d58df638d1b28
Kalle Kareinen <galezki@gmail.com>
2025-09-30 23:24:55 +0300

    Fix table

--- tables.py
+++ tables.py
@@ -4,18 +4,18 @@ import html
 from processing import format_seconds_to_ts
 
 def render_top_table(df):
-    df = df[["Time", "UUIW", "UUIW_msgs", "timestamp_url"]].copy()
-    df["Timestamp"] = df["Time"].apply(format_seconds_to_ts)
+    df = df[["Time", "timestamp_url", "UUIW", "UUIW_msgs"]].copy()
+    df["Time"] = df["Time"].apply(format_seconds_to_ts)
     df["timestamp_url"] = df["timestamp_url"].apply(lambda x: f"<a href='{x}' target='_blank'>🔗</a>")
     df["UUIW_msgs"] = df["UUIW_msgs"].apply(lambda x: html.escape(str(x)))
 
     df = df.rename(
         columns={
             "Time": "⏱️",
-            "Timestamp": "",
-            "UUIW": "Unique Users",
-            "UUIW_msgs": "💌",
+            "Timestamp": "⏱️ (s)",
             "timestamp_url": "🔗",
+            "UUIW": "Unique Users",
+            "UUIW_msgs": "💌"
         }
     )
 
